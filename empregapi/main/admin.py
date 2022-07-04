283b2a3832d20afb6605b7dcb3784551cf42f953
Shreya211120 <70424098+Shreya211120@users.noreply.github.com>
2022-07-04 17:27:21 +0530

    Employee commit

---
+++ empregapi/main/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Employee
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=['full_name','email','contact','address']
+
+admin.site.register(Employee,EmployeeAdmin)
+
+# Register your models here.
